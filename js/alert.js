c1d5a4c525ba982c5c4f48f33139e6be210f972f
Anatoly Nikiforov <tniks.work@gmail.com>
2020-06-18 08:58:04 +0300

    decktop fin

---
+++ js/alert.js
@@ -0,0 +1,9 @@
+let buttonBuy = document.getElementById('buy')
+
+buttonBuy.addEventListener('click', function() {
+  swal({
+  title: "ВАШИ ФОБИИ УЖЕ ЖДУТ ВАС В КОРЗИНЕ!",
+  text: "НО НЕ БОЛЬШЕ УПОКОВКИ В ОДНИ РУКИ, А ТО СОВСЕМ ПЛОХО БУДЕТ",
+  button: "ЗАКРЫТЬ",
+})
+})
